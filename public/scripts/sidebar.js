2c2518b14f5f774ed31f82a8b88cb3ca9c4afbd6
LuiosD <luisdartayet@gmail.com>
2022-04-19 17:02:54 -0300

    some more slides

--- public/scripts/sidebar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sidebar = document.querySelector('.sidebar');
-    const burgerSidebar = document.querySelector('.burger-sidebar');
-    burgerSidebar.addEventListener('click', () => {
-        sidebar.classList.toggle('sidebar-open');
-    });
-});
